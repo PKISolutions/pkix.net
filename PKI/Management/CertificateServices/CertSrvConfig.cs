68db11f6c291bff1c008295ee211631de5e8146b
Vadims Podāns <vpodans@sysadmins.lv>
2021-02-04 19:33:19 +0200

    early draft for new CA config infrastructure

---
+++ PKI/Management/CertificateServices/CertSrvConfig.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using PKI.CertificateServices;
+using PKI.Exceptions;
+
+namespace SysadminsLV.PKI.Management.CertificateServices {
+    /// <summary>
+    /// Represents base class for Certification Authority configuration. This is abstract class and cannot be instantiated directly.
+    /// </summary>
+    public abstract class CertSrvConfig {
+        /// <summary>
+        /// Initializes a new instance of <strong>CertSrvConfig</strong> from a string that contains a computer name where Certification Authority is installed.
+        /// </summary>
+        /// <param name="computerName">computer name where Certification Authority is installed.</param>
+        /// <exception cref="ArgumentException"><strong>computerName</strong> parameter cannot be null or empty string.</exception>
+        /// <exception cref="ServerUnavailableException">Certification Authority is not accessible via any supported protocol.</exception>
+        protected CertSrvConfig(String computerName) {
+            if (String.IsNullOrWhiteSpace(computerName)) {
+                throw new ArgumentException(nameof(computerName));
+            }
+
+            ComputerName = computerName;
+            ConfigManager = new CertSrvConfigUtil(ComputerName);
+
+            if (!ConfigManager.RegistryOnline && !ConfigManager.DcomOnline) {
+                var e = new ServerUnavailableException(ComputerName);
+                e.Data.Add(nameof(e.Source), OfflineSource.All);
+
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Gets the certification authority computer name.
+        /// </summary>
+        public String ComputerName { get; }
+        /// <summary>
+        /// Indicates whether the object was modified after it was instantiated. This member is set to <strong>False</strong> upon successful changes commit.
+        /// </summary>
+        public Boolean IsModified { get; protected set; }
+        /// <summary>
+        /// Gets the CA configuration read/write manager used by implementers to read and write configuration.
+        /// </summary>
+        protected CertSrvConfigUtil ConfigManager { get; }
+
+        /// <summary>
+        /// Updates Certification Authority configuration on a server.
+        /// </summary>
+        /// <param name="restart">
+        ///		Indicates whether to restart certificate services to immediately apply changes. Updated settings has no effect
+        ///		until CA service is restarted.
+        /// </param>
+        /// <exception cref="ServerUnavailableException">
+        ///		The target CA server could not be contacted via remote registry and RPC protocol.
+        /// </exception>
+        /// <returns>
+        ///		<strong>True</strong> if configuration was changed. If an object was not modified since it was instantiated, configuration is not updated
+        ///		and the method returns <strong>False</strong>.
+        /// </returns>
+        /// <remarks>
+        ///		The caller must have <strong>Administrator</strong> permissions on the target CA server.
+        /// </remarks>
+        public Boolean Commit(Boolean restart) {
+            if (!IsModified) {
+                return IsModified;
+            }
+
+            IsModified = false;
+            if (restart) {
+                CertificateAuthority.Restart(ComputerName);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PKI/Management/CertificateServices/CertSrvCryptographyConfig.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using SysadminsLV.PKI.Cryptography;
+
+namespace SysadminsLV.PKI.Management.CertificateServices {
+    /// <summary>
+    /// Represents a Certification Authority cryptography configuration which determines which provider and algorithms to use when
+    /// CA server signs certificates and certificate revocation lists (CRLs) and algorithm to use when CA creates CA exchange certificate
+    /// and performs key archival.
+    /// </summary>
+    public class CertSrvCryptographyConfig : CertSrvConfig {
+        const String S_NODE = "CSP";
+        const String E_NODE = "EncryptionCSP";
+
+        CspProviderInfo sigProv, encProv;
+        Boolean alternateSignatureAlgorithm;
+        Oid sigPubKeyAlg, encPubKeyAlg, hashingAlgorithm, encSymmetricAlg;
+        Int32 encPubKeyLength;
+
+        /// <summary>
+        /// Initializes a new instance of <strong>CertSrvCryptographyConfig</strong> using Certification Authority computer name.
+        /// </summary>
+        /// <param name="computerName">NetBIOS or FQDN name.</param>
+        public CertSrvCryptographyConfig(String computerName) : base(computerName) {
+            ConfigManager.SetRootNode(true);
+            readSigningCrypto();
+            readEncryptionCrypto();
+        }
+
+        /// <summary>
+        /// Gets or sets the public key algorithm (such as RSA, DSA or ECC) that is used for signing purposes.
+        /// </summary>
+        /// <remarks>Setter is ignored if provider specified in <see cref="EncryptionProvider"/> property is legacy provider.</remarks>
+        public Oid SigningPublicKeyAlgorithm {
+            get => sigPubKeyAlg;
+            set {
+                if (sigProv.IsLegacy && sigPubKeyAlg != null && value.Value == sigPubKeyAlg.Value) {
+                    return;
+                }
+                sigPubKeyAlg = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the hashing algorithm that is used for signing purposes.
+        /// </summary>
+        public Oid HashingAlgorithm {
+            get => hashingAlgorithm;
+            set {
+                if (hashingAlgorithm != null && value.Value == hashingAlgorithm.Value || !validateHashAlgorithm(value.Value)) {
+                    return;
+                }
+                hashingAlgorithm = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the value that indicates whether the CA server supports alternate signature algorithms (PKCS#1 v2.1)
+        /// </summary>
+        /// <remarks>Setter is ignored if provider specified in <see cref="EncryptionProvider"/> property is legacy provider.</remarks>
+        public Boolean AlternateSignatureAlgorithm {
+            get => alternateSignatureAlgorithm;
+            set {
+                if (value == alternateSignatureAlgorithm && sigProv.IsLegacy) {
+                    return;
+                }
+                alternateSignatureAlgorithm = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the provider name that is used by a Certification Authority installation.
+        /// </summary>
+        public CspProviderInfo SigningProvider {
+            get => sigProv;
+            set {
+                if (value == null || sigProv.Name.Equals(value.Name, StringComparison.OrdinalIgnoreCase)) {
+                    return;
+                }
+
+                sigProv = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the public key algorithm (such as RSA, DSA or ECC) that is used to generate CA Exchange encryption certificate.
+        /// Use <strong>ECDH_*</strong> for ECC-based public key. 
+        /// </summary>
+        /// <remarks>Setter is ignored if provider specified in <see cref="EncryptionProvider"/> property is legacy provider.</remarks>
+        public Oid EncryptionPublicKeyAlgorithm {
+            get => encPubKeyAlg;
+            set {
+                if (sigProv.IsLegacy && encPubKeyAlg != null && value.Value == encPubKeyAlg.Value) {
+                    return;
+                }
+                encPubKeyAlg = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the CA Exchange public key length.
+        /// </summary>
+        /// <remarks>Setter is applicable only when CA uses RSA public key algorithm and setter value is ignored if CA uses ECC public key.</remarks>
+        public Int32 EncryptionPublicKeySize {
+            get => encPubKeyLength;
+            set {
+                if (encPubKeyLength == value) {
+                    return;
+                }
+
+                encPubKeyLength = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the encryption symmetric algorithm used to perform key archival encryption.
+        /// </summary>
+        public Oid EncryptionAlgorithm {
+            get => encSymmetricAlg;
+            set {
+                if (encSymmetricAlg != null && value.Value == encSymmetricAlg.Value || !validateEncAlgorithm(value.Value)) {
+                    return;
+                }
+                encSymmetricAlg = value;
+                IsModified = true;
+            }
+        }
+        /// <summary>
+        /// Gets the length of symmetric encryption key. This property is automatically updated when setting <see cref="EncryptionAlgorithm"/> member.
+        /// </summary>
+        public Int32 SymmetricKeyLength { get; private set; }
+        /// <summary>
+        /// Gets or sets the provider name that is used by a Certification Authority installation.
+        /// </summary>
+        public CspProviderInfo EncryptionProvider {
+            get => encProv;
+            set {
+                if (value == null || encProv.Name.Equals(value.Name, StringComparison.OrdinalIgnoreCase)) {
+                    return;
+                }
+
+                encProv = value;
+                IsModified = true;
+            }
+        }
+
+        void readSigningCrypto() {
+            sigProv = CspProviderInfoCollection.GetProviderInfo(ConfigManager.GetStringEntry("Provider", S_NODE));
+            sigPubKeyAlg = sigProv.IsLegacy
+                ? new Oid(AlgorithmOid.RSA)
+                : new Oid(ConfigManager.GetStringEntry("CNGPublicKeyAlgorithm", S_NODE));
+            if (sigProv.IsLegacy) {
+                // legacy
+                hashingAlgorithm = getSigOidFromAlgId(ConfigManager.GetNumericEntry("HashAlgorithm", S_NODE));
+            } else {
+                // CNG
+                hashingAlgorithm = new Oid(ConfigManager.GetStringEntry("CNGHashAlgorithm", S_NODE));
+                alternateSignatureAlgorithm = ConfigManager.GetBooleanEntry("AlternateSignatureAlgorithm", S_NODE);
+            }
+        }
+        void readEncryptionCrypto() {
+            // encryption crypto
+            encProv = CspProviderInfoCollection.GetProviderInfo(ConfigManager.GetStringEntry("Provider", E_NODE));
+            if (encProv.IsLegacy) {
+                // Win2k3
+                encPubKeyAlg = new Oid(AlgorithmOid.RSA);
+                encSymmetricAlg = new Oid(AlgorithmOid.TrippleDES);
+            } else {
+                encPubKeyAlg = new Oid(ConfigManager.GetStringEntry("CNGPublicKeyAlgorithm", E_NODE));
+                encSymmetricAlg = new Oid(ConfigManager.GetStringEntry("CNGEncryptionAlgorithm", E_NODE));
+            }
+
+            encPubKeyLength = ConfigManager.GetNumericEntry("KeySize", E_NODE);
+            SymmetricKeyLength = ConfigManager.GetNumericEntry("SymmetricKeySize", E_NODE);
+        }
+
+        static Oid getSigOidFromAlgId(Int32 algId) {
+            switch (algId) {
+                case 0x8001: return new Oid(AlgorithmOid.MD2);
+                case 0x8003: return new Oid(AlgorithmOid.MD5);
+                case 0x8004: return new Oid(AlgorithmOid.SHA1);
+                case 0x8012: return new Oid(AlgorithmOid.SHA256);
+                case 0x8013: return new Oid(AlgorithmOid.SHA384);
+                case 0x8014: return new Oid(AlgorithmOid.SHA512);
+                default: return null;
+            }
+        }
+        static Int32 getSigAlgIdFromOid(Oid oid) {
+            switch (oid.Value) {
+                case AlgorithmOid.MD2: return 0x8001;
+                case AlgorithmOid.MD5: return 0x8003;
+                case AlgorithmOid.SHA1: return 0x8004;
+                case AlgorithmOid.SHA256: return 0x8012;
+                case AlgorithmOid.SHA384: return 0x8013;
+                case AlgorithmOid.SHA512: return 0x8014;
+                default: return 0;
+            }
+        }
+        static Oid getEncOidFromAlgId(Int32 algId) {
+            switch (algId) {
+                case 0x8001: return new Oid(AlgorithmOid.MD2);
+                case 0x8003: return new Oid(AlgorithmOid.MD5);
+                case 0x8004: return new Oid(AlgorithmOid.SHA1);
+                case 0x8012: return new Oid(AlgorithmOid.SHA256);
+                case 0x8013: return new Oid(AlgorithmOid.SHA384);
+                case 0x8014: return new Oid(AlgorithmOid.SHA512);
+                default: return null;
+            }
+        }
+        static Int32 getEncAlgIdFromOid(Oid oid) {
+            switch (oid.Value) {
+                case AlgorithmOid.DES: return 0x8001;
+                case AlgorithmOid.TrippleDES: return 0x8003;
+                case AlgorithmOid.AES128: return 0x8004;
+                case AlgorithmOid.AES192: return 0x8012;
+                case AlgorithmOid.AES256: return 0x8013;
+                default: return 0;
+            }
+        }
+
+        static Boolean validateHashAlgorithm(String value) {
+            return new List<String> {
+                                        AlgorithmOid.MD2,
+                                        AlgorithmOid.MD5,
+                                        AlgorithmOid.SHA1,
+                                        AlgorithmOid.SHA256,
+                                        AlgorithmOid.SHA384,
+                                        AlgorithmOid.SHA512
+                                    }.Contains(value);
+        }
+        static Boolean validateEncAlgorithm(String value) {
+            return new List<String> {
+                                        AlgorithmOid.DES,
+                                        AlgorithmOid.TrippleDES,
+                                        AlgorithmOid.AES128,
+                                        AlgorithmOid.AES192,
+                                        AlgorithmOid.AES256
+                                    }.Contains(value);
+        }
+    }
+}
