3e2a3d71dc8581303e19b8233e55f704946fee31
Vadims Podāns <vpodans@sysadmins.lv>
2022-03-22 12:22:47 +0200

    added missing Convert.ToInt32 method on authentication conversion to string

--- PKI/Dcom/Implementations/CertConfigEnrollEndpointD.cs
+++ PKI/Dcom/Implementations/CertConfigEnrollEndpointD.cs
@@ -23,8 +23,9 @@ namespace SysadminsLV.PKI.Dcom.Implementations {
         public Int32 Priority { get; }
         public Boolean RenewalOnly { get; }
         public Boolean KeyBasedRenewal { get; }
+
         public String DsEncode() {
-            return $"{Priority}\n{Authentication}\n{Convert.ToInt32(RenewalOnly)}\n{Uri}\n{Convert.ToInt32(KeyBasedRenewal)}";
+            return $"{Priority}\n{Convert.ToInt32(Authentication)}\n{Convert.ToInt32(RenewalOnly)}\n{Uri}\n{Convert.ToInt32(KeyBasedRenewal)}";
         }
     }
 }
