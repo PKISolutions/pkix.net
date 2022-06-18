f16b44d80405f61cedf03534df112660e685106e
Vadims Podāns <vpodans@sysadmins.lv>
2022-06-18 22:58:17 +0300

    addressed https://github.com/PKISolutions/PSPKI/issues/76 in backing library

---
+++ PKI/Dcom/ICertRequestAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SysadminsLV.PKI.Dcom {
+    public interface ICertRequestAdmin {
+        /// <summary>
+        /// Sets or modifies the custom extension for a pending request.
+        /// </summary>
+        /// <param name="requestID">Specifies the request ID.</param>
+        /// <param name="extension">Specifies the configured extension to set or modify.</param>
+        void SetCertificateExtension(Int32 requestID, X509Extension extension);
+    }
+}
