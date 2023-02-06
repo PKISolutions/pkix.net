3a1ecdabb264e145f69de4ca04b84c6e3ba696a5
Vadims Podāns <vpodans@sysadmins.lv>
2023-02-06 11:25:50 +0200

    updated xml-docs

--- PKI/Utils/CLRExtensions/X509Certificate2Extensions.cs
+++ PKI/Utils/CLRExtensions/X509Certificate2Extensions.cs
@@ -20,40 +20,40 @@ namespace SysadminsLV.PKI.Utils.CLRExtensions {
     /// </summary>
     public static class X509Certificate2Extensions {
         /// <summary>
-        /// Converts generic X.509 extension objects to specialized certificate extension objects
-        /// inherited from <see cref="X509Extension"/> class that provide extension-specific information.
+        ///     Converts generic X.509 extension objects to specialized certificate extension objects
+        ///     inherited from <see cref="X509Extension"/> class that provide extension-specific information.
         /// </summary>
         /// <param name="cert">Certificate.</param>
         /// <exception cref="ArgumentNullException">
-        /// <strong>cert</strong> parameter is null reference.
+        ///     <strong>cert</strong> parameter is null reference.
         /// </exception>
         /// <returns>A collection of certificate extensions</returns>
         /// <remarks>
-        /// This method can transform the following X.509 certificate extensions:
+        ///     This method can transform the following X.509 certificate extensions:
         /// <list type="bullet">
-        /// <item><description><see cref="X509CertificateTemplateExtension"/></description></item>
-        /// <item><description><see cref="X509ApplicationPoliciesExtension"/></description></item>
-        /// <item><description><see cref="X509ApplicationPolicyMappingsExtension"/></description></item>
-        /// <item><description><see cref="X509ApplicationPolicyConstraintsExtension"/></description></item>
-        /// <item><description><see cref="X509AuthorityInformationAccessExtension"/></description></item>
-        /// <item><description><see cref="X509NonceExtension"/></description></item>
-        /// <item><description><see cref="X509CRLReferenceExtension"/></description></item>
-        /// <item><description><see cref="X509ArchiveCutoffExtension"/></description></item>
-        /// <item><description><see cref="X509ServiceLocatorExtension"/></description></item>
-        /// <item><description><see cref="X509SubjectKeyIdentifierExtension"/></description></item>
-        /// <item><description><see cref="X509KeyUsageExtension"/></description></item>
-        /// <item><description><see cref="X509SubjectAlternativeNamesExtension"/></description></item>
-        /// <item><description><see cref="X509IssuerAlternativeNamesExtension"/></description></item>
-        /// <item><description><see cref="X509BasicConstraintsExtension"/></description></item>
-        /// <item><description><see cref="X509CRLNumberExtension"/></description></item>
-        /// <item><description><see cref="X509NameConstraintsExtension"/></description></item>
-        /// <item><description><see cref="X509CRLDistributionPointsExtension"/></description></item>
-        /// <item><description><see cref="X509CertificatePoliciesExtension"/></description></item>
-        /// <item><description><see cref="X509CertificatePolicyMappingsExtension"/></description></item>
-        /// <item><description><see cref="X509AuthorityKeyIdentifierExtension"/></description></item>
-        /// <item><description><see cref="X509CertificatePolicyConstraintsExtension"/></description></item>
-        /// <item><description><see cref="X509EnhancedKeyUsageExtension"/></description></item>
-        /// <item><description><see cref="X509FreshestCRLExtension"/></description></item>
+        ///     <item><see cref="X509ApplicationPoliciesExtension"/></item>
+        ///     <item><see cref="X509ApplicationPolicyConstraintsExtension"/></item>
+        ///     <item><see cref="X509ApplicationPolicyMappingsExtension"/></item>
+        ///     <item><see cref="X509ArchiveCutoffExtension"/></item>
+        ///     <item><see cref="X509AuthorityInformationAccessExtension"/></item>
+        ///     <item><see cref="X509CertificateTemplateExtension"/></item>
+        ///     <item><see cref="X509CRLReferenceExtension"/></item>
+        ///     <item><see cref="X509NonceExtension"/></item>
+        ///     <item><see cref="X509ServiceLocatorExtension"/></item>
+        ///     <item><see cref="X509SubjectKeyIdentifierExtension"/></item>
+        ///     <item><see cref="X509KeyUsageExtension"/></item>
+        ///     <item><see cref="X509SubjectAlternativeNamesExtension"/></item>
+        ///     <item><see cref="X509IssuerAlternativeNamesExtension"/></item>
+        ///     <item><see cref="X509BasicConstraintsExtension"/></item>
+        ///     <item><see cref="X509CRLNumberExtension"/></item>
+        ///     <item><see cref="X509NameConstraintsExtension"/></item>
+        ///     <item><see cref="X509CRLDistributionPointsExtension"/></item>
+        ///     <item><see cref="X509CertificatePoliciesExtension"/></item>
+        ///     <item><see cref="X509CertificatePolicyMappingsExtension"/></item>
+        ///     <item><see cref="X509AuthorityKeyIdentifierExtension"/></item>
+        ///     <item><see cref="X509CertificatePolicyConstraintsExtension"/></item>
+        ///     <item><see cref="X509EnhancedKeyUsageExtension"/></item>
+        ///     <item><see cref="X509FreshestCRLExtension"/></item>
         /// </list>
         /// Non-supported extensions will be returned as an <see cref="X509Extension"/> object.
         /// </remarks>
