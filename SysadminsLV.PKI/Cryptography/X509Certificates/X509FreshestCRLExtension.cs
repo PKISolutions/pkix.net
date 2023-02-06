63e392d138ccc50d83bab9fa215ee31724f6a058
Vadims Podāns <vpodans@sysadmins.lv>
2023-02-06 16:32:13 +0200

    moved System.Security.Cryptography.X509Certificates.X509FreshestCRLExtension to SysadminsLV.PKI.Cryptography.X509Certificates.X509FreshestCRLExtension (breaking)

--- SysadminsLV.PKI/Cryptography/X509Certificates/X509FreshestCRLExtension.cs
+++ SysadminsLV.PKI/Cryptography/X509Certificates/X509FreshestCRLExtension.cs
@@ -8,25 +8,27 @@ using SysadminsLV.Asn1Parser;
 namespace SysadminsLV.PKI.Cryptography.X509Certificates;
 
 /// <summary>
-/// Defines CRL Distribution Points (CDP) extension. This extension is used by a certificate chaining engine
-/// to validate the certificate revocation status. Normally, this extension contains URLs to a issuer CRL
-/// locations. 
+/// Defines Freshest or Delta CRL (certificate revocation list) extension. This extension is used by a
+/// certificate chaining engine to locate Delta CRL locations.
 /// </summary>
-public sealed class X509CRLDistributionPointsExtension : X509Extension {
-    static readonly Oid _oid = new(X509ExtensionOid.CRLDistributionPoints);
+/// <remarks>Delta CRLs are </remarks>
+public sealed class X509FreshestCRLExtension : X509Extension {
+    static readonly Oid _oid = new(X509ExtensionOid.FreshestCRL);
     readonly List<X509DistributionPoint> _distPoints = new();
 
-    internal X509CRLDistributionPointsExtension(Byte[] rawData, Boolean critical) : base(_oid, rawData, critical) {
-        if (rawData == null) { throw new ArgumentNullException(nameof(rawData)); }
+    X509FreshestCRLExtension(Byte[] rawData, Boolean critical) : base(_oid, rawData, critical) {
+        if (rawData == null) {
+            throw new ArgumentNullException(nameof(rawData));
+        }
         m_decode(rawData);
     }
 
     /// <summary>
-    /// Initializes a new instance of the <strong>X509CRLDistributionPointsExtension</strong> class.
+    /// Initializes a new instance of the <strong>X509FreshestCRLExtension</strong> class.
     /// </summary>
-    public X509CRLDistributionPointsExtension() { Oid = _oid; }
+    public X509FreshestCRLExtension() { Oid = _oid; }
     /// <summary>
-    /// Initializes a new instance of the <see cref="X509CRLDistributionPointsExtension"/> class using an
+    /// Initializes a new instance of the <see cref="X509FreshestCRLExtension"/> class using an
     /// <see cref="AsnEncodedData"/> object and a value that identifies whether the extension is critical.
     /// </summary>
     /// <param name="distributionPoints">The encoded data to use to create the extension.</param>
@@ -36,23 +38,25 @@ public sealed class X509CRLDistributionPointsExtension : X509Extension {
     /// <exception cref="ArgumentException">
     ///		The data in the <strong>distributionPoints</strong> parameter is not valid extension value.
     /// </exception>
-    public X509CRLDistributionPointsExtension(AsnEncodedData distributionPoints, Boolean critical) :
+    public X509FreshestCRLExtension(AsnEncodedData distributionPoints, Boolean critical) :
         this(distributionPoints.RawData, critical) { }
     /// <summary>
-    /// Initializes a new instance of the <strong>X509CRLDistributionPointsExtension</strong> class by using
-    /// array of URL strings.
+    /// Initializes a new instance of the <strong>X509FreshestCRLExtension</strong> class by using array
+    /// of URL strings.
     /// </summary>
-    /// <param name="urls">An array of CDP URLs.</param>
-    /// <exception cref="ArgumentNullException"><strong>urls</strong>> parameter is null.</exception>
-    public X509CRLDistributionPointsExtension(String[] urls) {
-        if (urls == null) { throw new ArgumentNullException(nameof(urls)); }
+    /// <param name="urls">An array of Delta CRL URLs.</param>
+    /// <exception cref="ArgumentNullException"><strong>url</strong>> parameter is null.</exception>
+    public X509FreshestCRLExtension(String[] urls) {
+        if (urls == null) {
+            throw new ArgumentNullException(nameof(urls));
+        }
         m_initialize(urls);
     }
 
     /// <summary>
     /// Gets CRL Distribution Points URLs.
     /// </summary>
-    public X509DistributionPoint[] CRLDistributionPoints => _distPoints.ToArray();
+    public X509DistributionPoint[] FreshestCrlDistributionPoints => _distPoints.ToArray();
 
     void m_initialize(IEnumerable<String> urls) {
         Oid = _oid;
@@ -65,7 +69,7 @@ public sealed class X509CRLDistributionPointsExtension : X509Extension {
         RawData = Asn1Utils.Encode(rawData.ToArray(), 48);
         RawData = Asn1Utils.Encode(RawData, 48);
     }
-    void m_decode (Byte[] rawData) {
+    void m_decode(Byte[] rawData) {
         var asn = new Asn1Reader(rawData);
         if (asn.Tag != 48) { throw new Asn1InvalidTagException(asn.Offset); }
         asn.MoveNext();
@@ -75,10 +79,10 @@ public sealed class X509CRLDistributionPointsExtension : X509Extension {
     }
 
     /// <summary>
-    /// Gets an array of certificate revocation list URLs listed in the extension.
+    /// Gets an array of Delta CRL URLs listed in the extension.
     /// </summary>
     /// <returns>An array of URLs.</returns>
-    public String[] GetURLs() {
+    public String[] GetUrLs() {
         return _distPoints
             .SelectMany(x => x.FullName)
             .Where(x => x.Type == X509AlternativeNamesEnum.URL)
