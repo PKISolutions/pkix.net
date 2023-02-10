67251bb96eba1eecb3232d50eead01191e135b66
Vadims Podāns <vpodans@sysadmins.lv>
2023-02-10 16:49:13 +0200

    removed unused method

--- PKI/Utils/GenericArray.cs
+++ PKI/Utils/GenericArray.cs
@@ -38,12 +38,5 @@ namespace PKI.Utils {
             extensions.RemoveAt(index);
             return true;
         }
-        public static void ReverseOrder(ref Byte[] array) {
-            for (Int32 index = 0; index < array.Length; index += 2) {
-                Byte temp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = temp;
-            }
-        }
     }
 }
