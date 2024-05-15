610313851e7f121007308348a67cbef4a79932e6
Vadims Podāns <vpodans@sysadmins.lv>
2024-05-15 12:53:32 +0300

    added defaults to nullable IX509CertificateTemplate properties

--- src/SysadminsLV.PKI.Win/Utils/CLRExtensions/CX509CertificateTemplateExtensions.cs
+++ src/SysadminsLV.PKI.Win/Utils/CLRExtensions/CX509CertificateTemplateExtensions.cs
@@ -7,39 +7,73 @@ namespace SysadminsLV.PKI.Utils.CLRExtensions;
 /// Contains extension methods for <strong>IX509CertificateTemplate</strong> COM interface.
 /// </summary>
 static class CX509CertificateTemplateExtensions {
-    public static Int32 GetInt32(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName) {
+    /// <summary>
+    /// Gets signed 32-bit integer from OLE object.
+    /// </summary>
+    /// <param name="template">Current template instance.</param>
+    /// <param name="propertyName">Property to retrieve.</param>
+    /// <param name="defaultValue">Optional default value to return if requested property doesn't exist or is empty.</param>
+    /// <returns>Signed 32-bit integer.</returns>
+    public static Int32 GetInt32(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName, Int32 defaultValue = default) {
         try {
             return Convert.ToInt32(template.Property[propertyName]);
         } catch {
-            return default;
+            return defaultValue;
         }
     }
-    public static Int64 GetInt64(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName) {
+    /// <summary>
+    /// Gets signed 64-bit integer from OLE object.
+    /// </summary>
+    /// <param name="template">Current template instance.</param>
+    /// <param name="propertyName">Property to retrieve.</param>
+    /// <param name="defaultValue">Optional default value to return if requested property doesn't exist or is empty.</param>
+    /// <returns>Signed 64-bit integer.</returns>
+    public static Int64 GetInt64(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName, Int64 defaultValue = default) {
         try {
             return Convert.ToInt64(template.Property[propertyName]);
         } catch {
-            return default;
+            return defaultValue;
         }
     }
-    public static TValue GetScalarValue<TValue>(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName) {
+    /// <summary>
+    /// Gets scalar value from OLE object.
+    /// </summary>
+    /// <param name="template">Current template instance.</param>
+    /// <param name="propertyName">Property to retrieve.</param>
+    /// <param name="defaultValue">Optional default value to return if requested property doesn't exist or is empty.</param>
+    /// <returns>Scalar value.</returns>
+    public static TValue GetScalarValue<TValue>(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName, TValue defaultValue = default) {
         try {
             return (TValue)template.Property[propertyName];
         } catch {
-            return default;
+            return defaultValue;
         }
     }
-    public static TEnum GetEnum<TEnum>(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName) where TEnum : struct, Enum {
+    /// <summary>
+    /// Gets enum value from 32-bit signed integer from OLE object.
+    /// </summary>
+    /// <param name="template">Current template instance.</param>
+    /// <param name="propertyName">Property to retrieve.</param>
+    /// <param name="defaultValue">Optional default value to return if requested property doesn't exist or is empty.</param>
+    /// <returns>Enum value.</returns>
+    public static TEnum GetEnum<TEnum>(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName, TEnum defaultValue = default) where TEnum : struct, Enum {
         try {
             return (TEnum)Enum.Parse(typeof(TEnum), Convert.ToInt32(template.Property[propertyName]).ToString());
         } catch {
-            return default;
+            return defaultValue;
         }
     }
+    /// <summary>
+    /// Gets a collection of scalar values from OLE object.
+    /// </summary>
+    /// <param name="template">Current template instance.</param>
+    /// <param name="propertyName">Property to retrieve.</param>
+    /// <returns>Scalar array.</returns>
     public static TValue[] GetCollectionValue<TValue>(this IX509CertificateTemplate template, EnrollmentTemplateProperty propertyName) {
         try {
             return (TValue[])template.Property[propertyName];
         } catch {
-            return Array.Empty<TValue>();
+            return [];
         }
     }
 }
