ad80aad93cb11ee680189d143d7940aefd019cf2
Vadims Podāns <vpodans@sysadmins.lv>
2024-05-16 14:28:16 +0300

    cleanup

--- src/SysadminsLV.PKI.Win/Dcom/Implementations/CertEnrollCertificateTemplate.cs
+++ src/SysadminsLV.PKI.Win/Dcom/Implementations/CertEnrollCertificateTemplate.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 using Interop.CERTENROLLLib;
 using PKI.CertificateTemplates;
 using SysadminsLV.PKI.CertificateTemplates;
