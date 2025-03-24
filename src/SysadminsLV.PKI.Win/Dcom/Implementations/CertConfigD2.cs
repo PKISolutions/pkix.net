d2a8371606e8a009aecaddb76c20ae0cf8bb1f43
Vadims Podāns <vpodans@sysadmins.lv>
2025-03-24 15:47:16 +0200

    replaced manual  Marshal.FinalReleaseComObject with CryptographyUtils.ReleaseCom

--- src/SysadminsLV.PKI.Win/Dcom/Implementations/CertConfigD2.cs
+++ src/SysadminsLV.PKI.Win/Dcom/Implementations/CertConfigD2.cs
@@ -1,87 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using CERTCLILib;
+using SysadminsLV.PKI.Utils;
 
-namespace SysadminsLV.PKI.Dcom.Implementations {
-    /// <summary>
-    /// Represents a static Windows implementation for <see cref="ICertConfigD"/> interface.
-    /// </summary>
-    public static class  CertConfigD2 {
-        static String getConfig(CertConfigOption option) {
-            var certConfig = new CCertConfigClass();
+namespace SysadminsLV.PKI.Dcom.Implementations; 
+/// <summary>
+/// Represents a static Windows implementation for <see cref="ICertConfigD"/> interface.
+/// </summary>
+public static class  CertConfigD2 {
+    static String getConfig(CertConfigOption option) {
+        var certConfig = new CCertConfigClass();
+        try {
+            return certConfig.GetConfig((Int32)option);
+        } catch {
+            return null;
+        } finally {
+            CryptographyUtils.ReleaseCom(certConfig);
+        }
+    }
+
+    /// <inheritdoc cref="ICertConfigD.GetDefaultConfig"/>
+    public static String GetDefaultConfig() {
+        return getConfig(CertConfigOption.DefaultConfig);
+    }
+    /// <inheritdoc cref="ICertConfigD.GetFirstConfig"/>
+    public static String GetFirstConfig() {
+        return getConfig(CertConfigOption.FirstConfig);
+    }
+    /// <inheritdoc cref="ICertConfigD.GetLocalConfig"/>
+    public static String GetLocalConfig() {
+        return getConfig(CertConfigOption.LocalConfig);
+    }
+    /// <inheritdoc cref="ICertConfigD.GetLocalActiveConfig"/>
+    public static String GetLocalActiveConfig() {
+        return getConfig(CertConfigOption.LocalActiveConfig);
+    }
+    /// <inheritdoc cref="ICertConfigD.GetUIConfig"/>
+    public static String GetUIConfig() {
+        return getConfig(CertConfigOption.UIPickConfig);
+    }
+    /// <inheritdoc cref="ICertConfigD.GetUISkipLocalConfig"/>
+    public static String GetUISkipLocalConfig() {
+        return getConfig(CertConfigOption.UIPickConfigSkipLocalCA);
+    }
+    /// <inheritdoc cref="ICertConfigD.EnumConfigEntries"/>
+    public static ICertConfigEntryD[] EnumConfigEntries() {
+        var list = new List<ICertConfigEntryD>();
+        var certConfig = new CCertConfigClass();
+        while (certConfig.Next() >= 0) {
+            list.Add(new CertConfigEntryD(certConfig));
+        }
+        CryptographyUtils.ReleaseCom(certConfig);
+        return list.ToArray();
+    }
+    /// <inheritdoc cref="ICertConfigD.FindConfigEntryByCertificateName"/>
+    public static ICertConfigEntryD FindConfigEntryByCertificateName(String caName) {
+        var certConfig = new CCertConfigClass();
+
+        while (certConfig.Next() >= 0) {
             try {
-                return certConfig.GetConfig((Int32)option);
-            } catch {
-                return null;
-            } finally {
-                Marshal.FinalReleaseComObject(certConfig);
-            }
+                if (certConfig.GetField("CommonName").Equals(caName, StringComparison.CurrentCultureIgnoreCase)) {
+                    var entry = new CertConfigEntryD(certConfig);
+                    CryptographyUtils.ReleaseCom(certConfig);
+                    return entry;
+                }
+            } catch { }
         }
+        return null;
+    }
+    /// <inheritdoc cref="ICertConfigD.FindConfigEntryByServerName"/>
+    public static ICertConfigEntryD FindConfigEntryByServerName(String computerName) {
+        var certConfig = new CCertConfigClass();
 
-        /// <inheritdoc cref="ICertConfigD.GetDefaultConfig"/>
-        public static String GetDefaultConfig() {
-            return getConfig(CertConfigOption.DefaultConfig);
+        while (certConfig.Next() >= 0) {
+            try {
+                if (certConfig.GetField("Server").Equals(computerName, StringComparison.OrdinalIgnoreCase)) {
+                    var entry = new CertConfigEntryD(certConfig);
+                    CryptographyUtils.ReleaseCom(certConfig);
+                    return entry;
+                }
+            } catch { }
         }
-        /// <inheritdoc cref="ICertConfigD.GetFirstConfig"/>
-        public static String GetFirstConfig() {
-            return getConfig(CertConfigOption.FirstConfig);
-        }
-        /// <inheritdoc cref="ICertConfigD.GetLocalConfig"/>
-        public static String GetLocalConfig() {
-            return getConfig(CertConfigOption.LocalConfig);
-        }
-        /// <inheritdoc cref="ICertConfigD.GetLocalActiveConfig"/>
-        public static String GetLocalActiveConfig() {
-            return getConfig(CertConfigOption.LocalActiveConfig);
-        }
-        /// <inheritdoc cref="ICertConfigD.GetUIConfig"/>
-        public static String GetUIConfig() {
-            return getConfig(CertConfigOption.UIPickConfig);
-        }
-        /// <inheritdoc cref="ICertConfigD.GetUISkipLocalConfig"/>
-        public static String GetUISkipLocalConfig() {
-            return getConfig(CertConfigOption.UIPickConfigSkipLocalCA);
-        }
-        /// <inheritdoc cref="ICertConfigD.EnumConfigEntries"/>
-        public static ICertConfigEntryD[] EnumConfigEntries() {
-            var list = new List<ICertConfigEntryD>();
-            var certConfig = new CCertConfigClass();
-            while (certConfig.Next() >= 0) {
-                list.Add(new CertConfigEntryD(certConfig));
-            }
-            Marshal.FinalReleaseComObject(certConfig);
-            return list.ToArray();
-        }
-        /// <inheritdoc cref="ICertConfigD.FindConfigEntryByCertificateName"/>
-        public static ICertConfigEntryD FindConfigEntryByCertificateName(String caName) {
-            var certConfig = new CCertConfigClass();
-
-            while (certConfig.Next() >= 0) {
-                try {
-                    if (certConfig.GetField("CommonName").Equals(caName, StringComparison.CurrentCultureIgnoreCase)) {
-                        var entry = new CertConfigEntryD(certConfig);
-                        Marshal.FinalReleaseComObject(certConfig);
-                        return entry;
-                    }
-                } catch { }
-            }
-            return null;
-        }
-        /// <inheritdoc cref="ICertConfigD.FindConfigEntryByServerName"/>
-        public static ICertConfigEntryD FindConfigEntryByServerName(String computerName) {
-            var certConfig = new CCertConfigClass();
-
-            while (certConfig.Next() >= 0) {
-                try {
-                    if (certConfig.GetField("Server").Equals(computerName, StringComparison.OrdinalIgnoreCase)) {
-                        var entry = new CertConfigEntryD(certConfig);
-                        Marshal.FinalReleaseComObject(certConfig);
-                        return entry;
-                    }
-                } catch { }
-            }
-            return null;
-        }
+        return null;
     }
 }
