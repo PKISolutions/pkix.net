6f5124d3313d83a34393f2402637970efb1504a5
Vadims Podāns <vpodans@sysadmins.lv>
2025-04-21 18:46:07 +0300

    it seems that classes are not needed since we have a COM interface with CoClass attribute. Go with it.

--- src/SysadminsLV.PKI.Win/Dcom/Implementations/CertAdminFactory.cs
+++ src/SysadminsLV.PKI.Win/Dcom/Implementations/CertAdminFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using CERTADMINLib;
+﻿using CERTADMINLib;
 // ReSharper disable SuspiciousTypeConversion.Global
 
 namespace SysadminsLV.PKI.Dcom.Implementations;
@@ -9,43 +8,15 @@ namespace SysadminsLV.PKI.Dcom.Implementations;
 /// </summary>
 static class CertAdminFactory {
     public static ICertAdmin2 CreateICertAdmin() {
-        return (ICertAdmin2)new CCertAdminClass();
+        return new CCertAdmin();
     }
     public static ICertView2 CreateICertView() {
-        return (ICertView2)new CCertViewClass();
+        return new CCertView();
     }
     public static IOCSPAdmin CreateIOCSPAdmin() {
-        return (IOCSPAdmin)new OCSPAdminClass();
+        return new OCSPAdmin();
     }
     public static IOCSPPropertyCollection CreateOCSPPropertyCollection() {
-        return (IOCSPPropertyCollection)new OCSPPropertyCollectionClass();
+        return new OCSPPropertyCollection();
     }
-
-    #region COM classes
-
-    [Guid("37EABAF0-7FB6-11D0-8817-00A0C903B83C")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class CCertAdminClass;
-
-    [Guid("A12D0F7A-1E84-11D1-9BD6-00C04FB683FA")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class CCertViewClass;
-
-    [Guid("D3F73511-92C9-47CB-8FF2-8D891A7C4DE4")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class OCSPAdminClass;
-
-    [Guid("F935A528-BA8A-4DD9-BA79-F283275CB2DE")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class OCSPPropertyCollectionClass;
-
-    #endregion
 }
