647f7193d6d3155b3b582d910cbbbfce27db7bab
Vadims Podāns <vpodans@sysadmins.lv>
2025-04-21 18:46:07 +0300

    it seems that classes are not needed since we have a COM interface with CoClass attribute. Go with it.

--- src/SysadminsLV.PKI.Win/Dcom/Implementations/CertCliFactory.cs
+++ src/SysadminsLV.PKI.Win/Dcom/Implementations/CertCliFactory.cs
@@ -1,38 +1,15 @@
-﻿using System.Runtime.InteropServices;
-using CERTCLILib;
+﻿using CERTCLILib;
 
 namespace SysadminsLV.PKI.Dcom.Implementations;
 
 static class CertCliFactory {
     public static ICertConfig2 CreateCertConfig() {
-        return (ICertConfig2)new CCertConfigClass();
+        return new CCertConfig();
     }
     public static ICertGetConfig CreateCertGetConfig() {
-        return (ICertGetConfig)new CCertGetConfigClass();
+        return new CCertGetConfig();
     }
     public static ICertRequest3 CreateCertRequest() {
-        return (ICertRequest3)new CCertRequestClass();
+        return new CCertRequest();
     }
-
-    #region COM classes
-
-    [Guid("372FCE38-4324-11D0-8810-00A0C903B83C")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class CCertConfigClass;
-
-    [Guid("C6CC49B0-CE17-11D0-8833-00A0C903B83C")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class CCertGetConfigClass;
-
-    [Guid("98AFF3F0-5524-11D0-8812-00A0C903B83C")]
-    [TypeLibType(TypeLibTypeFlags.FCanCreate)]
-    [ClassInterface(ClassInterfaceType.None)]
-    [ComImport]
-    class CCertRequestClass;
-
-    #endregion
 }
