2f3836cbd1905a5ddce179bc49d73897d88a791b
Vadims Podāns <vpodans@sysadmins.lv>
2025-04-21 15:47:12 +0300

    created COM classes for CERTADMINLib and added factory class

---
+++ src/SysadminsLV.PKI.Win/Dcom/Implementations/CertAdminFactory.cs
@@ -0,0 +1,22 @@
+﻿using CERTADMINLib;
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace SysadminsLV.PKI.Dcom.Implementations;
+
+/// <summary>
+/// Represents factory class for CERTADMINLib COM classes.
+/// </summary>
+static class CertAdminFactory {
+    public static ICertAdmin2 CreateICertAdmin() {
+        return (ICertAdmin2)new CCertAdminClass();
+    }
+    public static ICertView2 CreateICertView() {
+        return (ICertView2)new CCertViewClass();
+    }
+    public static IOCSPAdmin CreateIOCSPAdmin() {
+        return (IOCSPAdmin)new OCSPAdminClass();
+    }
+    public static IOCSPPropertyCollection CreateOCSPPropertyCollection() {
+        return (IOCSPPropertyCollection)new OCSPPropertyCollectionClass();
+    }
+}
